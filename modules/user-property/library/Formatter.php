52835330ccedd73e7907c2c981b4ac18ee1e8e14
Iqbal Fauzi <iqbalfawz@gmail.com>
2017-10-20 18:16:48 +0700

    Fix minor bug on format type not exists

--- modules/user-property/library/Formatter.php
+++ modules/user-property/library/Formatter.php
@@ -23,16 +23,12 @@ class Formatter
             $config = $dis->config->formatter['user'];
             
             $properties = $dis->config->user_property;
-            foreach($properties as $prop => $args)
-                $config[$prop] = $args['format'];
-            
+            foreach($properties as $prop => $args){
+                if(isset($args['format']))
+                    $config[$prop] = $args['format'];
+            }
             $dis->config->set('formatter', 'user-property', $config);
-        }else{
-            $config = $dis->config->formatter['user-property'];
         }
-        
-        $new_props = array_diff($config, $dis->config->formatter['user']);
-        $new_props = array_combine(array_keys($new_props), array_fill(0, count($new_props), NULL));
         
         $ids = array_column($objects, 'id');
         $props = UProperty::get(['user'=>$ids], true);
@@ -41,6 +37,9 @@ class Formatter
         foreach($props as $uid => $prs)
             $props[$uid] = array_column($prs, 'value', 'name');
 
+        $new_props = array_keys($dis->config->user_property);
+        $new_props = array_combine($new_props, array_fill(0, count($new_props), null));
+        
         foreach($objects as $index => $object){
             $uprops = $props[$object->id] ?? [];
             $uprops = array_replace($new_props, $uprops);
